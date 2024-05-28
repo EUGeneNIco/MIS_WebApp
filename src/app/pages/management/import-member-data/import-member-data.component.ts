98506a8b2c2e56ffefb8baf3a157b89678800a0e
Eugene Hermano <eunico.hermano@gmail.com>
2024-05-28 22:54:58 +0800

    Importing excel in ui (partial)

--- src/app/pages/management/import-member-data/import-member-data.component.ts
+++ src/app/pages/management/import-member-data/import-member-data.component.ts
@@ -3,15 +3,16 @@ import { MessageService } from 'primeng/api';
 import { AppPageBaseComponent } from 'src/app/_components/base/app-page-base.component';
 import { MasterBaseComponent } from 'src/app/_components/base/master-base.component';
 import { NotificationService } from 'src/app/services/notification.service';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-import-member-data',
   templateUrl: './import-member-data.component.html'
 })
 export class ImportMemberDataComponent extends AppPageBaseComponent implements OnInit {
+  excelData: any;
 
-
-  uploadedFiles: any[] = [];
+  uploadedData: any[] = [];
   constructor(
     private messageService: MessageService,
     notifService: NotificationService
@@ -24,7 +25,7 @@ export class ImportMemberDataComponent extends AppPageBaseComponent implements O
 
   onUpload(event: any) {
     for (const file of event.files) {
-      this.uploadedFiles.push(file);
+      this.uploadedData.push(file);
     }
 
     this.messageService.add({ severity: 'info', summary: 'Success', detail: 'File Uploaded' });
@@ -35,5 +36,19 @@ export class ImportMemberDataComponent extends AppPageBaseComponent implements O
       { label: 'Management' },
       { label: 'Import Data', url: '' }
     ]);
+  }
+
+  readExcel(event) {
+    let file = event.target.files[0];
+
+    let fileReader = new FileReader();
+    fileReader.readAsBinaryString(file);
+    fileReader.onload = (e) => {
+      var wb = XLSX.read(fileReader.result, { type: 'binary' });
+      var sheetNames = wb.SheetNames;
+      this.excelData = XLSX.utils.sheet_to_json(wb.Sheets[sheetNames[0]]);
+      console.log(this.excelData);
+    }
+
   }
 }
